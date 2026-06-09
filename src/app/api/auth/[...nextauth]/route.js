11fbb3c5b647695abeccb0e629a78b29017ed0d7
kudoma84-pixel <kudoma84@gmail.com>
2026-06-09 09:46:43 +0900

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -12,6 +12,12 @@ const handler = NextAuth({
     signIn: '/src',
   },
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.includes('/src')) {
+        return baseUrl + '/src?notify=line&openExternalBrowser=1';
+      }
+      return url;
+    },
     async jwt({ token, account }) {
       if (account) {
         token.lineUserId = account.providerAccountId;
